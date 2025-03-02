9d277de21d5e9934628ba19d137af421a6805686
Guilherme Maciel <guilhermemcl1503@gmail.com>
2025-03-02 19:22:50 -0300

    integrate OpenAI for itinerary creation; update environment configuration

--- src/controllers/itineraryControllers.ts
+++ src/controllers/itineraryControllers.ts
@@ -1,11 +1,15 @@
+import { env } from "@/env";
 import { prisma } from "@/lib/prisma";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { JwtPayload } from "jsonwebtoken";
+import OpenAI from "openai";
 import z from "zod";
 
 
 export class ItineraryControllers {
     async createItinerary(request: FastifyRequest, reply: FastifyReply) {
+        const openai = new OpenAI({
+            apiKey: env.OPENAI_API_KEY
+        });
         const parmsSchema = z.object({
             id: z.string()
         })
@@ -19,9 +23,44 @@ export class ItineraryControllers {
             }
         })
 
-        return console.log(trip)
+
+        if (!trip) {
+            return reply.status(404).send({ message: "Viagem nao encontrada | Trip not found" })
+        }
+
+
+
+        try {
+            const response = await openai.chat.completions.create({
+                model: "gpt-4",
+                messages: [
+                    {
+                        role: "system", content: "voce é um assistente profissional de viagens que cria roteiros detalhados "
+                    },
+                    {
+                        role: "user", content: `crie um roteiro detalhado de viagem com pontos turisticos e atividades recomendadas para ${trip.city} entre ${trip.startDate} e ${trip.endDate}, coloque os custo de gasto medio das atividades e alimentação`
+                    }
+                ]
+            })
+            const itinerary = response.choices[0].message.content || "Roteiro nao criado | Itinerary not created"
+
+            const updateTrip = await prisma.trip.update({
+                where: {
+                    id
+                },
+                data: {
+                    itinerary
+                }
+            })
+
+            return reply.send({ updateTrip }.updateTrip).code(201)
+
+
+        } catch (error) {
+            return reply.status(500).send({ message: "Erro ao criar roteiro | Error creating itinerary" })
+        }
 
 
     }
-
 }
+
--- src/routes/itinerary-routes.ts
+++ src/routes/itinerary-routes.ts
@@ -1,11 +1,9 @@
 import { ItineraryControllers } from "@/controllers/itineraryControllers";
-import { itineraryIA } from "@/middlewares/itinerary";
 import { FastifyInstance } from "fastify";
 
 const itineraryControllers = new ItineraryControllers();
 
 export async function itineraryRoutes(app: FastifyInstance) {
-    await itineraryIA(app)
     app.post("/trips/:id/itinerary", itineraryControllers.createItinerary)
 
 }
